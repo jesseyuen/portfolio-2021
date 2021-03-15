feaf34bd4df896ca1dedf8fd108abc7019539464
Jesse Yuen <hello@jesseyuen.com>
2021-03-15 19:49:09 +0000

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -47,12 +47,12 @@ module.exports = function(config) {
   const livePosts = post => post.date <= now && !post.data.draft;
   config.addCollection('posts', collection => {
     return [
-      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
+      ...collection.getFilteredByGlob('./src/thoughts/*.md').filter(livePosts)
     ].reverse();
   });
 
   config.addCollection('postFeed', collection => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
+    return [...collection.getFilteredByGlob('./src/thoughts/*.md').filter(livePosts)]
       .reverse()
       .slice(0, site.maxPostsPerPage);
   });
